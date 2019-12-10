52ebdec40f9878590fdff694758221043153f6cf
Arnak <arnak.melikyan@arpinet.am>
2019-12-10 16:06:56 +0400

    Changed facebook auth scope

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,7 @@ class UserAdmin(BaseUserAdmin):
     add_form_template = 'admin/add_form.html'
     fieldsets = (
         (None, {'fields': ('username', 'password', 'email')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'sex', 'is_subscriber', 'avatar')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'sex', 'is_subscriber', 'profile_url', 'avatar')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
