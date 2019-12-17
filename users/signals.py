b4e9598100f7ec6d28d517672181545beed89dfe
testHardSoft <tests@hardsoftrobotics.com>
2019-12-17 17:34:54 +0400

    Added signals to add facebook user profile url

--- users/signals.py
+++ users/signals.py
@@ -1,12 +1,12 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 
-@receiver(pre_save, sender=UserModel)
+@receiver(post_save, sender=UserModel)
 def add_facebook_profile_link(sender, instance, created, **kwargs):
-    if created:
+    if instance.pk:
         data = instance.social_auth.all().values('extra_data')[0]
         instance.link = data['extra_data']['link']
         instance.save()
